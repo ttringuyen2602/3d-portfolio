3de34d99029460eda12b7b060b92cc47129462ef
Tri Nguyen <ttringuyen2602@gmail.com>
2023-03-20 09:24:18 +0700

    write about for some component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
